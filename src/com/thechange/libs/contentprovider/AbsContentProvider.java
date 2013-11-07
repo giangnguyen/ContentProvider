4cac7e76783f84618cf73bb9e51a5cff54d4b36e
Giang Nguyen <giang.nguyentruong.uit@gmail.com>
2013-11-07 11:11:52 -0800

    remove sqlcipher

--- src/com/thechange/libs/contentprovider/AbsContentProvider.java
+++ src/com/thechange/libs/contentprovider/AbsContentProvider.java
@@ -1,12 +1,12 @@
 package com.thechange.libs.contentprovider;
 
-import net.sqlcipher.Cursor;
-import net.sqlcipher.SQLException;
-import net.sqlcipher.database.SQLiteDatabase;
-import net.sqlcipher.database.SQLiteQueryBuilder;
 import android.content.ContentProvider;
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 
 import com.thechange.libs.dbtable.AbsDBTable;
